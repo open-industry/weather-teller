003e255984b2d7eaad3373f45d5b36384949f04b
anon-legion <g.sync.mobile1@gmail.com>
2022-02-10 14:44:40 +0800

    add file exclusion for eslint props-no-spreading in ForecastSlider.js

--- src/components/forecast/ForecastSlider.js
+++ src/components/forecast/ForecastSlider.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { useState, useLayoutEffect, useRef } from 'react';
 import { useSwipeable } from 'react-swipeable';
 import { motion } from 'framer-motion';
